2504982cb09ad073a9bb832728d53f1de1a9b48e
brandonburiii <brandonypf@maxsys.com.my>
2026-01-28 15:37:17 +0800

    added controller

---
+++ Testing water/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Testing_water.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int age { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public User() { }
+
+
+    }
+}
